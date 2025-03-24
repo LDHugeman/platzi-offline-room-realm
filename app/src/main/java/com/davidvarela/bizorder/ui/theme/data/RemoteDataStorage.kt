46ae1275f3b0fac89b61eef06f07977745dad49e
LDHugeman <luisdavid.vp@gmail.com>
2025-03-24 21:02:49 +0100

    Clase 5

---
+++ app/src/main/java/com/davidvarela/bizorder/ui/theme/data/RemoteDataStorage.kt
@@ -0,0 +1,26 @@
+package com.davidvarela.bizorder.ui.theme.data
+
+import javax.inject.Inject
+
+class RemoteDataStorage @Inject constructor(
+    val bizOrderApi: BizOrderApi
+) {
+
+    suspend fun getOrders() = runCatching {
+        val response = bizOrderApi.getOrders()
+        if (response.isSuccessful) {
+            response.body().orEmpty()
+        } else {
+            throw Exception("${response.code()}: ${response.message()}")
+        }
+    }
+
+    suspend fun savePreOrders() = runCatching {
+        val response = bizOrderApi.savePreOrders()
+        if (response.isSuccessful) {
+            response.body() ?: Unit
+        } else {
+            throw Exception("${response.code()}: ${response.message()}")
+        }
+    }
+}
